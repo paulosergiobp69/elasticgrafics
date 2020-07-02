32a655ba709480beb3b5f6ddf44a2fb3f7187ab7
paulosergiobp69 <58534338+paulosergiobp69@users.noreply.github.com>
2020-07-02 16:28:52 -0300

    CONSULTAS ARTIGOS FUNCIONANDO

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Articles;
-
+use Elasticsearch\Client;
+use Elasticsearch\ClientBuilder;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,13 +16,29 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //dd("Alo do Paulo");
+        $this->app->bind(Articles\ArticlesRepository::class, function ($app) {
+            // This is useful in case we want to turn-off our
+            // search cluster or when deploying the search
+            // to a live, running application at first.
+            if (! config('services.search.enabled')) {
+                return new Articles\EloquentRepository();
+            }
             
-        $this->app->bind(
-            Articles\ArticlesRepository::class,
-            Articles\EloquentRepository::class
-        );
+            return new Articles\ElasticsearchRepository(
+                $app->make(Client::class)
+            );
+        });
 
+        $this->bindSearchClient();
+    }
+
+    private function bindSearchClient()
+    {
+        $this->app->bind(Client::class, function ($app) {
+            return ClientBuilder::create()
+                ->setHosts($app['config']->get('services.search.hosts'))
+                ->build();
+        });
     }
 
     /**
